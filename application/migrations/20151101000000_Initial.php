1c94d957ea4e0cb1820e45459a7071733bd2bada
Alexandros Iordanidis <alexander_iordanidis@hotmail.com>
2015-11-12 21:57:45 +0200

    created initial migration for db

--- application/migrations/20151101000000_Initial.php
+++ application/migrations/20151101000000_Initial.php
@@ -10,7 +10,7 @@ class Migration_Initial extends CI_Migration {
 		/* TABLE: users */
 		$this->dbforge->add_field(array(
 			'record_id' => array(
-				'type' => 'BIGINT',
+				'type' => 'INT',
 				'unsigned' => TRUE,
 				'auto_increment' => TRUE
 			),
@@ -45,6 +45,160 @@ class Migration_Initial extends CI_Migration {
 		));
 		$this->dbforge->add_key('record_id', TRUE);
 		$this->dbforge->create_table('users', TRUE, $attributes);
+
+		/* TABLE: products */
+		$this->dbforge->add_field(array(
+			'record_id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'deleted_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'insert_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'update_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'category_record_id' => array(
+				'type' => 'INT',
+				'null' => TRUE,
+			),
+			'name' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 255,
+				'null' => TRUE,
+			),			
+			'description' => array(
+				'type' => 'TEXT',
+				'null' => TRUE,
+			)
+		));
+
+		$this->dbforge->add_key('record_id', TRUE);
+		$this->dbforge->create_table('products', TRUE, $attributes);
+
+		/* TABLE: product_categories */
+		$this->dbforge->add_field(array(
+			'record_id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'deleted_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'insert_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'update_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'name' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 255,
+				'null' => TRUE,
+			),			
+			'description' => array(
+				'type' => 'TEXT',
+				'null' => TRUE,
+			)
+		));
+
+		$this->dbforge->add_key('record_id', TRUE);
+		$this->dbforge->create_table('product_categories', TRUE, $attributes);
+
+		/* TABLE: shifts */
+		$this->dbforge->add_field(array(
+			'record_id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'deleted_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'insert_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'update_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'user_record_id' => array(
+				'type' => 'INT',
+				'null' => TRUE,
+			),			
+			'start_datetime' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE,
+			),			
+			'end_datetime' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE,
+			),
+			'total_sales' => array(
+				'type' => 'FLOAT',
+				'null' => TRUE,
+			)
+		));
+
+		$this->dbforge->add_key('record_id', TRUE);
+		$this->dbforge->create_table('shifts', TRUE, $attributes);
+
+		/* TABLE: orders */
+		$this->dbforge->add_field(array(
+			'record_id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'deleted_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'insert_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'update_at' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE
+			),
+			'user_record_id' => array(
+				'type' => 'INT',
+				'null' => TRUE,
+			),				
+			'shift_record_id' => array(
+				'type' => 'INT',
+				'null' => TRUE,
+			),				
+			'order_data' => array(
+				'type' => 'LONGBLOB',
+				'null' => TRUE,
+			),			
+			'start_datetime' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE,
+			),			
+			'end_datetime' => array(
+				'type' => 'DATETIME',
+				'null' => TRUE,
+			)
+		));
+
+		$this->dbforge->add_key('record_id',  TRUE);
+		$this->dbforge->create_table('orders', TRUE, $attributes);
 
 		$this->insert_common_records();
 	}
