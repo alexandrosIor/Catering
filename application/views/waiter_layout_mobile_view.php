4d8cf03dccde25df1829ed4797ab161a151743ee
Alexandros Iordanidis <alexander_iordanidis@hotmail.com>
2015-11-14 23:07:58 +0200

    made some changes to layout elements

--- application/views/waiter_layout_mobile_view.php
+++ application/views/waiter_layout_mobile_view.php
@@ -27,47 +27,71 @@
 		<!-- Make sure all your bars are the first things in your <body> -->
 		<header class="bar bar-nav">
 			<a class="icon icon-gear pull-right" href="#settingsModal"></a>
-			<h1 class="title">Catering</h1>
+			<?=isset($home) ? '<a href="' . $home . '" class="icon icon-left-nav pull-left" data-transition="slide-out"></a>' : ''?>
+			<h1 class="title"><?=$title?></h1>
 		</header>
 
 		<?=$body?>
 
-		<nav class="bar bar-tab">
-			<a class="tab-item" href="tables.php" data-transition="slide-in">
-				<!-- <span class="icon icon-person"></span> -->
-				<span class="icon"><i class="fa fa-th-large"></i></span>
-				<span class="tab-label">τραπέζια</span>
-			</a>
-			<a class="tab-item" href="#">
-				<!-- <span class="icon icon-star-filled"></span> -->
-				<span class="icon"><i class="fa fa-pencil-square-o"></i></span>
-				<span class="tab-label">παραγγελίες</span>
-			</a>
-			<a class="tab-item" href="#">
-				<span class="icon icon-search"></span>
-				<span class="tab-label">Search</span>
-			</a>
-			<a class="tab-item" href="#">
-				<span class="icon icon-gear"></span>
-				<span class="tab-label">Settings</span>
-			</a>
-			<a class="tab-item" href="#">
+		<!-- Settings Modal -->
+			<div id="settingsModal" class="modal">
+				<header class="bar bar-nav">
+					<a class="icon icon-close pull-right" href="#settingsModal"></a>
+					<h1 class="title">Ρυθμίσεις</h1>
+				</header>
 
-				<style>
-				.badge-notify{
-				   background:red;
-				   position:relative;
-				   top: -40px;
-				   left: 15px;
-				}
-				</style>
+				<div class="content">
+					<h5 class="content-padded">Ρυθμισεις λογαριασμου</h5>
+					<form class="input-group">
+						<input type="text" placeholder="Αλέξανδρος Ιορδανίδης">
+						<input type="email" placeholder="<?=$logged_in_member->email?>">
+						<div class="content-padded">
+							<button class="btn btn-positive btn-block" type="submit" disabled>αποθηκευση</button>
+						</div>
+					</form>
 
-				<div class="container">
-				<span class="icon"><i class="fa fa-bell-o"></i></span>
-					<span class="tab-label">Home
-					  <span class="badge badge-notify">3</span></span>
+					<h5 class="content-padded">App settings</h5>
+					<ul class="table-view">
+						<li class="table-view-cell media">
+							<span class="media-object pull-left icon icon-sound"></span>
+							<div class="media-body">
+								Enable sounds
+							</div>
+							<div class="toggle">
+								<div class="toggle-handle"></div>
+							</div>
+						</li>
+						<li class="table-view-cell media">
+							<span class="media-object pull-left icon icon-person"></span>
+							<div class="media-body">
+								Parental controls
+							</div>
+							<div class="toggle">
+								<div class="toggle-handle"></div>
+							</div>
+						</li>
+					</ul>
+
+					<h5 class="content-padded">πληροφοριες βαρδιας</h5>
+					<div class="content-padded">
+						<i class="fa fa-clock-o fa-lg"></i> 
+						<span> 4:32:20</span>
+					</div>
+
+					<div class="content-padded">
+						<a href="/logout" class="btn btn-negative btn-block sign-out" data-transition="slide-in">αποσύνδεση <i class="fa fa-sign-out fa-lg fa-fw"></i></a>
+					</div>
 				</div>
+			</div>
+
+		<nav class="bar bar-tab">
+<?php foreach ($menu as $key => $menu_item)
+{?>
+			<a class="tab-item" href="<?=$menu_item['link']?>" data-transition="slide-in">
+				<span class="icon"><i class="<?=$menu_item['icon']?>"></i></span>
+				<span class="tab-label"><?=$menu_item['name']?></span>
 			</a>
+<?php }?>
 		</nav>
 		
 		<!-- Include the compiled Ratchet JS -->
